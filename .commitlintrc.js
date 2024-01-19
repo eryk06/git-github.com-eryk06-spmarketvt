0bf5d0da954423acd58f3d5ce1e2674780d88e81
Nguyễn Trần Quang Lâm <lamnguyen062003@gmail.com>
2024-01-20 03:50:46 +0700

    fix: commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -15,14 +15,9 @@ module.exports = {
         'test', // Viết test
         'revert', // Revert lại commit trước đó
         'ci', // Thay đổi cấu hình CI/CD
-        'build' // Build tệp tin
-      ]
+        'build', // Build tệp tin
+      ],
     ],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'scope-empty': [2, 'never'],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
-  }
+    'header-max-length': [2, 'always', 72],
+  },
 };
